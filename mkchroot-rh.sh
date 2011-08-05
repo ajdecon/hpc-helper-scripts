99ba67cbceb9a7b76e2d2c29f21260924297a09e
Adam DeConinck <adeconinck@rsystemsinc.com>
2011-08-05 15:38:10 -0500

    Removed mdadm from the default image script.
    For some reason, on the PE1950's I've been using to test, a
    CentOS 6 or SL 6 image including mdadm and no configuration, is
    going completely stupid when it tries to access the (single) drive.
    Removing mdadm fixes this issue.

--- mkchroot-rh.sh
+++ mkchroot-rh.sh
@@ -63,7 +63,7 @@ yum --installroot $VNFSDIR -y install \
     sed setup shadow-utils sysklogd tcp_wrappers termcap tzdata util-linux \
     words zlib tar less gzip which util-linux module-init-tools udev \
     openssh-clients openssh-server passwd dhclient pciutils vim-minimal \
-    shadow-utils strace man OpenIPMI ipmitool mdadm perl python cronie \
+    shadow-utils strace man OpenIPMI ipmitool perl python cronie \
     crontabs readline iptables man sysstat tcpdump device-mapper
 
 
